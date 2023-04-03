39f70ac75b92ef2fe831ea0daac523aaad0d94bb
unknown <alex.gorulya@gmail.com>
2023-04-03 21:36:39 +0200

    commit_202

---
+++ 202_HappyNumber/HappyNumber.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        int n = 19;
+        long start = System.nanoTime();
+        boolean res = isHappy(n);
+        System.out.println(System.nanoTime() - start);
+        System.out.println(res);
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+           }
+//            while (n >= 10) n /= 10;
+ //           sum += n * n;
+            n = sum;
+        }
+        return n == 1;
+    }
+}

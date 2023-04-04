d6a7c4ada8961cf89ad4efd3f0d0799b07bbb3ce
unknown <alex.gorulya@gmail.com>
2023-04-04 13:05:35 +0200

    commit_205

---
+++ 205_IsomorphicStrings/IsomorphicStrings.java
@@ -0,0 +1,24 @@
+public class IsomorphicStrings {
+    public static void main(String[] args) {
+        String s = "badc";
+        String t = "baba";
+        boolean res = isIsomorphic(s, t);
+        System.out.println(res);
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        char alpha[] = new char[257];
+        boolean ar[] = new boolean[257];
+        char ss[] = s.toCharArray();
+        char tt[] = t.toCharArray();
+        for (int i = 0; i < ss.length; i++) {
+            if (alpha[tt[i]] == 0 && ar[ss[i]] == false) {
+                alpha[tt[i]] = ss[i];
+                ar[ss[i]] = true;
+            } else if (alpha[tt[i]] != ss[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
